e288429b7c7f112d0ccf570118e2ff101acd8f75
Marco Antonio Angel Galeana <marcoangelgaleana@outlook.com>
2022-12-21 19:08:05 -0500

    Tema 4

--- Tema.java
+++ Tema.java
@@ -1,27 +1,43 @@
 public class Tema {
     public static void main(String[] args) {
-        System.out.println(suma(5,9,8));
-        Coche coche=new Coche();
-        coche.incrementaPuertas();
-        System.out.println(coche.setPuertas());
-    }
-
-    public static int suma(int x, int z, int y){
-        return x+z+y;
-    }
-}
-
-class Coche{
-    private int puertas;
-    public Coche(){
-        puertas=0;
-    }
-
-    public int setPuertas(){
-        return puertas;
-    }
-
-    public void incrementaPuertas(){
-        puertas++;
+        int numeroIf=0, numeroWhile=-9, numeroDoWhile=0, numerfor=3, estacion=4;
+        if(numeroIf!=0){
+            if(numeroIf>0)
+                System.out.println("numeroIf es positivo");
+            else
+                System.out.println("numeroIf es negativo");
+        }
+        else{
+            System.out.println("numeroIf es 0");
+        }
+        while(numeroWhile<3){
+            numeroWhile++;
+            System.out.println("numeroWhile es: "+numeroWhile);
+        }
+        do{
+            numeroDoWhile++;
+            System.out.println("numeroDoWhile es: "+numeroDoWhile);
+        }
+        while(numeroDoWhile<0);
+        for(int i=0; i<=numerfor; i++){
+            System.out.println("numeroFor es: "+numerfor);
+        }
+        switch(estacion){
+            case 0:
+                System.out.println("Primavera");
+            break;
+            case 1:
+                System.out.println("Verano");
+            break;
+            case 2:
+                System.out.println("Otoño");
+            break;
+            case 3:
+                System.out.println("Invierno");
+            break;
+            default:
+            System.out.println("Error");
+            break;
+        }
     }
 }
